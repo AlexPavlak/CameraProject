ca48863aef0a83442684379b85586edf2841654c
Alex Pavlak <alex@alexpavlak.com>
2018-07-22 16:41:31 -0500

    Added basic elements of background subtraction.

--- Camera.py
+++ Camera.py
@@ -3,14 +3,18 @@ import cv2
 
 #Create a video capture object
 cap = cv2.VideoCapture(0)
+background = cv2.createBackgroundSubtractorMOG2()
 
 while(True):
 
     #Retrieve input from the camera frame by frame
     ret, frame = cap.read()
 
+    foreground = background.apply(frame)
+
     #Display the captured frames
     cv2.imshow('frame',frame)
+    cv2.imshow('foreground', foreground)
 
     #Continue displaying until q is pressed to break the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
